cc03079f90a628ee473420c393c7d69f30cde6c4
Daner SHARIFI <daner.sharifi.official@gmail.com>
2024-05-16 14:55:57 +0200

    HomePage : automatic disable for difficulties based on it's dimension

--- src/main/java/iut/prj2024/view/HomePageController.java
+++ src/main/java/iut/prj2024/view/HomePageController.java
@@ -1,17 +1,24 @@
 package iut.prj2024.view;
 
+import iut.prj2024.AkariApp;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class HomePageController implements Initializable {
     private Stage stage;
 
+    @FXML
+    private ToggleGroup dimensions;
     @FXML
     private RadioButton dimension3x3RadioButton;
     @FXML
@@ -26,6 +33,8 @@ public class HomePageController implements Initializable {
     private RadioButton dimension14x14RadioButton;
 
     @FXML
+    private ToggleGroup difficulty;
+    @FXML
     private RadioButton easyRadioButton;
     @FXML
     private RadioButton hardRadioButton;
@@ -39,68 +48,81 @@ public class HomePageController implements Initializable {
     @FXML
     public void startButtonAction() {
         if (dimension3x3RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 3, 3, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 3, 3, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 3, 3, 3);
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else if (dimension5x5RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 5, 5, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 5, 5, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 5, 5, 3);
-            } else {
-                //new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else if (dimension6x4RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 6, 4, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 6, 4, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 6, 4, 3);
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else if (dimension7x7RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 7, 7, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 7, 7, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 7, 7, 3);
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else if (dimension10x10RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 10, 10, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 10, 10, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 10, 10, 3);
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else if (dimension14x14RadioButton.isSelected()) {
-            if (easyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 14, 14, 1);
-            } else if (hardRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 14, 14, 2);
-            } else if (trickyRadioButton.isSelected()) {
-                //new GamePageController().startGame(stage, 14, 14, 3);
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Please select a difficulty level").show();
-            }
+
         } else {
             new Alert(Alert.AlertType.ERROR, "Please select a dimension").show();
         }
+    }
+
+    @FXML
+    public void checkDifficultyAvailable() {
+        if (dimensions.getSelectedToggle() != null) {
+            String dimension = dimensions.getSelectedToggle().toString();
+            dimension = dimension.substring(dimension.indexOf("'") + 1);
+            dimension = dimension.substring(0, dimension.indexOf("'"));
+
+            String[] levels = availableLevels(dimension, false);
+            System.out.println(Arrays.toString(levels));
+            this.easyRadioButton.setDisable(levels[0] == null);
+            if (this.easyRadioButton.isSelected() && levels[0] == null) {
+                this.easyRadioButton.setSelected(false);
+            }
+            this.hardRadioButton.setDisable(levels[1] == null);
+            if (this.hardRadioButton.isSelected() && levels[1] == null) {
+                this.hardRadioButton.setSelected(false);
+            }
+            this.trickyRadioButton.setDisable(levels[2] == null);
+            if (this.trickyRadioButton.isSelected() && levels[2] == null) {
+                this.trickyRadioButton.setSelected(false);
+            }
+        }
+    }
+
+
+    private String[] availableLevels(String dimension, boolean showAllFiles) {
+        File folder = new File(pathToDimension(dimension));
+        File[] files = folder.listFiles((dir, name) -> name.startsWith("easy") || name.startsWith("hard") || name.startsWith("tricky"));
+        if (files == null) {
+            System.out.println("No levels found");
+            System.out.println(folder.getAbsolutePath());
+            return new String[0];
+        }
+        String[] levels = new String[files.length];
+        if (showAllFiles) {
+            for (int i = 0; i < files.length; i++) {
+                levels[i] = files[i].getName();
+            }
+        } else {
+            levels = new String[3];
+            if (Arrays.stream(files).anyMatch(file -> file.getName().startsWith("easy"))) {
+                levels[0] = "easy";
+            }
+            if (Arrays.stream(files).anyMatch(file -> file.getName().startsWith("hard"))) {
+                levels[1] = "hard";
+            }
+            if (Arrays.stream(files).anyMatch(file -> file.getName().startsWith("tricky"))) {
+                levels[2] = "tricky";
+            }
+        }
+        return levels;
+    }
+
+    private String pathToDimension(String dimension) {
+        return "src/main/resources/iut/prj2024/jeu/dataset/" + dimension;
+    }
+
+    private String pathToLevel(String dimension, String difficulty) {
+        return pathToDimension(dimension) + "/" + difficulty;
     }
 
     @FXML
